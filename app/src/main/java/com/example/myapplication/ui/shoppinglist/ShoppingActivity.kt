c5d42ca431e31bab6ae0cfd6ffe425294f624cf9
prodige <mateoosh_tar@o2.pl>
2022-12-15 15:51:46 +0100

    Zrobiony viewmodel

--- app/src/main/java/com/example/myapplication/ui/shoppinglist/ShoppingActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/shoppinglist/ShoppingActivity.kt
@@ -5,14 +5,17 @@ import android.os.Bundle
 import androidx.lifecycle.ViewModelProviders
 import com.example.myapplication.R
 import com.example.myapplication.data.db.ShoppingDatabase
+import com.example.myapplication.data.repositories.ShoppingRepository
 
 class ShoppingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shopping)
 
+        val database = ShoppingDatabase(this)
+        val repository = ShoppingRepository(database)
+        val factory = ShoppingViewModelFactory(repository)
 
-
-        val viewModel = ViewModelProviders.of(this).get(ShoppingViewModel::class.java)
+        val viewModel = ViewModelProviders.of(this, factory).get(ShoppingViewModel::class.java)
     }
 }
