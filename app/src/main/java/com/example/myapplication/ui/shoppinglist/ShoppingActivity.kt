dafab85bce0c7bb900f3580ca34902b92326bf6f
prodige <mateoosh_tar@o2.pl>
2022-12-16 15:57:02 +0100

    po zrobieniu compatDialog

--- app/src/main/java/com/example/myapplication/ui/shoppinglist/ShoppingActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/shoppinglist/ShoppingActivity.kt
@@ -2,10 +2,15 @@ package com.example.myapplication.ui.shoppinglist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.myapplication.R
 import com.example.myapplication.data.db.ShoppingDatabase
+import com.example.myapplication.data.db.entities.ShoppingItem
 import com.example.myapplication.data.repositories.ShoppingRepository
+import com.example.myapplication.other.ShoppingItemAdapter
+import kotlinx.android.synthetic.main.activity_shopping.*
 
 class ShoppingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +23,24 @@ class ShoppingActivity : AppCompatActivity() {
 
         val viewModel = ViewModelProviders.of(this, factory).get(ShoppingViewModel::class.java)
 
+        val adapter = ShoppingItemAdapter(listOf(), viewModel)
+
+        rvShoppingItems.layoutManager = LinearLayoutManager(this)
+        rvShoppingItems.adapter = adapter
+
+        viewModel.getAllShoppingItems().observe(this, Observer {
+            adapter.items = it
+            adapter.notifyDataSetChanged()
+        })
+
+        fab.setOnClickListener {
+            AddShoppingItemDialog(this,
+            object : AddDialogListener {
+                override fun onAddButtonClicked(item: ShoppingItem) {
+
+                }
+            })
+        }
 
     }
 }
